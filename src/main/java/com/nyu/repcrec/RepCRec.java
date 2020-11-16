8bb2d00c18acb2909ad88dc7a88c6916a235592e
shivkhattar <shivkhattar94@gmail.com>
2020-11-16 14:42:31 -0500

    Initial Commit

--- src/main/java/com/nyu/repcrec/RepCRec.java
+++ src/main/java/com/nyu/repcrec/RepCRec.java
@@ -1,6 +1,15 @@
 package com.nyu.repcrec;
 
+import com.nyu.repcrec.service.Operation;
+import com.nyu.repcrec.service.TransactionManager;
+
+import java.util.List;
+
 public class RepCRec {
+
+    private List<Operation> operations;
+    private TransactionManager transactionManager;
+
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Please provide an input!");
