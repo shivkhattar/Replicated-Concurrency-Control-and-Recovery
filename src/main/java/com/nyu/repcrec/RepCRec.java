87cc72cd8f38e0180b08853f1bb0ac7689e154cd
Abhinav Gupta <guptabhi123@gmail.com>
2020-11-24 17:57:35 -0500

    added all input test files

--- src/main/java/com/nyu/repcrec/RepCRec.java
+++ src/main/java/com/nyu/repcrec/RepCRec.java
@@ -11,8 +11,7 @@ public class RepCRec {
 
     public static void main(String[] args) {
         try {
-            for(int i = 0; i < args.length; i++){
-                Integer inputFileNumber = Integer.parseInt(args[i]);
+            for(String inputFileNumber : args){
                 FileUtils.log(String.format(Constants.INPUT_FILE, inputFileNumber));
                 executeInputFile(inputFileNumber);
                 FileUtils.log(Constants.ASTERISK_LINE);
@@ -26,7 +25,7 @@ public class RepCRec {
 
     }
 
-    public static void executeInputFile(int fileNumber) throws Exception{
+    public static void executeInputFile(String fileNumber) throws Exception{
         FileUtils.createOutputFile(Constants.RESOURCE_DIR_PATH + "/output", "output-" + fileNumber);
         List<Operation> operations = FileUtils.parseFile(Constants.RESOURCE_DIR_PATH + "/input" + "/input-" + fileNumber);
         executeOperations(operations);
